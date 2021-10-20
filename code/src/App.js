deedf2936c32036e4872111946dca121804708ce
Kauã Maia Aquino <54816886+kaquino22@users.noreply.github.com>
2021-10-20 19:45:15 -0300

    Subindo código

---
+++ code/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import Calculadora from './components/calculadora/calculadora';
+
+function App() {
+  return (
+    <div>
+     <Calculadora/>
+    </div>
+  );
+}
+
+export default App;
